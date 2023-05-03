d24c583e37cb7db54c51482449149b0394338a9a
Emma Kang <emma140626@gmail.com>
2023-05-03 00:37:12 -0400

    Update custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,8 +3,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        electric: '#db00ff',
-        ribbon: '#0047ff',
+        space: '#22223B',
+        ultra: '#4A4E69',
+        rose: '#9A8C98',
+        pale: '#C9ADA7',
+        info: '#00B8D9',
+        warn: '#FFAB00',
+        error: '#FF5630',
+        success: '#36B37E'
       },
     },
   },
