3faeb6e6a77c7bdb7ee4f7c64206f9c7ffc78912
Emma Kang <emma140626@gmail.com>
2023-05-09 16:35:34 -0400

    Remove unused style and component

--- src/components/Input.tsx
+++
@@ -1,8 +0,0 @@
-import tw, { styled } from 'twin.macro';
-
-const Input = styled.input(({ hasError }: { hasError: boolean }) => [
-  tw`p-2 border rounded w-full`,
-  hasError ? tw`border-rose` : tw`border-gray-600`,
-]);
-
-export default Input;
